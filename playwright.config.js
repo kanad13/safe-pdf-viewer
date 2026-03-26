2a33c0dedb4d18adfbcbaf8cb5e5547edaa86bc0
Kunal Pathak <kunal@safe-pdf-viewer>
2026-03-26 19:27:20 +0100

    chore: formatting applied to playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,24 +1,24 @@
 // @ts-check
-const { defineConfig, devices } = require('@playwright/test');
+const { defineConfig, devices } = require("@playwright/test");
 
 module.exports = defineConfig({
-  testDir: './test/e2e',
-  fullyParallel: true,
-  retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
-  use: {
-    trace: 'on-first-retry',
-  },
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-  ],
-  webServer: {
-    command: 'node test/e2e/fixtures/server.js',
-    port: 8080,
-    reuseExistingServer: !process.env.CI,
-  },
+	testDir: "./test/e2e",
+	fullyParallel: true,
+	retries: process.env.CI ? 2 : 0,
+	workers: process.env.CI ? 1 : undefined,
+	reporter: "html",
+	use: {
+		trace: "on-first-retry",
+	},
+	projects: [
+		{
+			name: "chromium",
+			use: { ...devices["Desktop Chrome"] },
+		},
+	],
+	webServer: {
+		command: "node test/e2e/fixtures/server.js",
+		port: 8080,
+		reuseExistingServer: !process.env.CI,
+	},
 });
